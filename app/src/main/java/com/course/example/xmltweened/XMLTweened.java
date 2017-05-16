5468aa03b10c8bdc0e7089ce2c66bdaa44a7bbfe
Jim Pepe <jim.pepe@verizon.net>
2017-05-16 15:03:11 -0400

    Initial commit

---
+++ app/src/main/java/com/course/example/xmltweened/XMLTweened.java
@@ -0,0 +1,56 @@
+/*
+ * A relative layout contains an ImageView and a TextView widget overlaying
+ * the ImageView using a z-index. The layout is animated although individual 
+ * widgets can also be animated.
+ */
+
+package com.course.example.xmltweened;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
+import android.widget.ImageView;
+
+public class XMLTweened extends Activity {
+    
+	private RelativeLayout layout;
+	private ImageView image;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        layout = (RelativeLayout)findViewById(R.id.layout);
+        image = (ImageView) findViewById(R.id.img);
+        image.setImageResource(R.drawable.shape);
+		
+		// Load the appropriate animation
+        Animation an =  AnimationUtils.loadAnimation(this, R.anim.shakeitup);
+        // Register a listener, so we can disable and re-enable buttons
+        an.setAnimationListener(new MyAnimationListener());	
+        // Start the animation
+        layout.startAnimation(an);	
+        
+        
+    }
+    
+    class MyAnimationListener implements Animation.AnimationListener {
+
+		public void onAnimationEnd(Animation animation) {
+			// what to do when animation ends
+		}
+		public void onAnimationRepeat(Animation animation) {
+			// what to do when animation loops 
+		}
+
+		public void onAnimationStart(Animation animation) {			
+			// what to do when animation starts
+		}
+		
+	}
+    
+}
